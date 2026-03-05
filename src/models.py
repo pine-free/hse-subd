235c0441f654c58038fe85a6682499bd15b18df2
Alexander Kurkotov <alexander.kurkotov@kaspersky.com>
2026-03-05 12:34:32 +0300

    renamed tables to lowercase

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class Base(DeclarativeBase):
     pass
 
 class Drinks(Base):
-    __tablename__ = "Drinks"
+    __tablename__ = "drinks"
 
     drink_ID: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[Optional[str]] = mapped_column(String(20))
@@ -18,21 +18,21 @@ class Drinks(Base):
     category: Mapped[Optional[str] ]= mapped_column(String(20))
 
 class BarSupplies(Base):
-    __tablename__ = "Bar_supplies"
+    __tablename__ = "bar_supplies"
 
     quantity: Mapped[Optional[int]]
-    drink_ID: Mapped[int] = mapped_column(ForeignKey("Drinks.drink_ID"), primary_key=True)
+    drink_ID: Mapped[int] = mapped_column(ForeignKey("drinks.drink_ID"), primary_key=True)
 
 class Orders(Base):
-    __tablename__ = "Orders"
+    __tablename__ = "orders"
 
     order_ID: Mapped[int] = mapped_column(primary_key=True)
     total: Mapped[Optional[int]]
     order_time: Mapped[Optional[datetime.datetime]]
-    staff_ID: Mapped[int] = mapped_column(ForeignKey("Bartenders.staff_ID"))
+    staff_ID: Mapped[int] = mapped_column(ForeignKey("bartenders.staff_ID"))
 
 class Staff(Base):
-    __tablename__ = "Staff"
+    __tablename__ = "staff"
 
     staff_ID: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     name: Mapped[Optional[str]] = mapped_column(String(20))
@@ -41,26 +41,26 @@ class Staff(Base):
     age: Mapped[Optional[int]]
 
 class Bartenders(Base):
-    __tablename__ = "Bartenders"
+    __tablename__ = "bartenders"
     __table_args__ = (CheckConstraint("performance_rating >= 0 AND performance_rating <= 5"),)
 
-    staff_ID: Mapped[int] = mapped_column(ForeignKey("Staff.staff_ID"), primary_key=True)
+    staff_ID: Mapped[int] = mapped_column(ForeignKey("staff.staff_ID"), primary_key=True)
     performance_rating: Mapped[Optional[int]]
 
 class Dealers(Base):
-    __tablename__ = "Dealers"
+    __tablename__ = "dealers"
         
-    staff_ID: Mapped[int] = mapped_column(ForeignKey("Staff.staff_ID"), primary_key=True)
+    staff_ID: Mapped[int] = mapped_column(ForeignKey("staff.staff_ID"), primary_key=True)
     qualification: Mapped[Optional[str]] = mapped_column(String(20))
 
 class Security(Base):
-    __tablename__ = "Security"
+    __tablename__ = "security"
 
-    staff_ID: Mapped[int] = mapped_column(ForeignKey("Staff.staff_ID"), primary_key=True)
+    staff_ID: Mapped[int] = mapped_column(ForeignKey("staff.staff_ID"), primary_key=True)
     zone: Mapped[Optional[int]]
 
 class Clients(Base):
-    __tablename__ = "Clients"
+    __tablename__ = "clients"
 
     user_ID: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[Optional[str]] = mapped_column(String(20))
@@ -73,59 +73,59 @@ class Clients(Base):
     
 
 class Card(Base):
-    __tablename__ = "Card"
+    __tablename__ = "card"
 
     card_ID: Mapped[int] = mapped_column(primary_key=True)
     user_ID: Mapped[Optional[int]]
     balance: Mapped[Optional[int]]
 
-class CardToClientsDispenser(Base):
-    __tablename__ = "CardToClientsDispenser"
+class CardsToClientsDispenser(Base):
+    __tablename__ = "cards_to_clients_dispenser"
 
     dispenser_ID: Mapped[int] = mapped_column(primary_key=True)
-    user_ID: Mapped[int] = mapped_column(ForeignKey("Clients.user_ID"))
-    card_ID: Mapped[int] = mapped_column(ForeignKey("Card.card_ID"))
+    user_ID: Mapped[int] = mapped_column(ForeignKey("clients.user_ID"))
+    card_ID: Mapped[int] = mapped_column(ForeignKey("card.card_ID"))
     entry_time: Mapped[Optional[datetime.datetime]]
     abandon_time: Mapped[Optional[datetime.datetime]]
 
 
 class SplitOrderByCard(Base):
-    __tablename__ = "Split_Order_by_Card"
+    __tablename__ = "split_order_by_card"
 
     split_order_ID: Mapped[int] = mapped_column(primary_key=True)
-    card_ID: Mapped[int] = mapped_column(ForeignKey("Card.card_ID"))
-    order_ID: Mapped[int] = mapped_column(ForeignKey("Orders.order_ID"))
-    drink_ID: Mapped[int] = mapped_column(ForeignKey("Drinks.drink_ID"))
+    card_ID: Mapped[int] = mapped_column(ForeignKey("card.card_ID"))
+    order_ID: Mapped[int] = mapped_column(ForeignKey("orders.order_ID"))
+    drink_ID: Mapped[int] = mapped_column(ForeignKey("drinks.drink_ID"))
     quantity: Mapped[Optional[int]]
 
 class Session(Base):
-    __tablename__ = "Session"
+    __tablename__ = "session"
 
     session_ID: Mapped[int] = mapped_column(primary_key=True)
     start_time: Mapped[Optional[datetime.datetime]]
     end_time: Mapped[Optional[datetime.datetime]]
 
 class CardBidWithnSession(Base):
-    __tablename__ = "Card_Bid_within_Session"
+    __tablename__ = "card_bid_within_session"
 
     bid_ID: Mapped[int] = mapped_column(primary_key=True)
-    session_ID: Mapped[int] = mapped_column(ForeignKey("Session.session_ID"))
-    card_ID: Mapped[int] = mapped_column(ForeignKey("Card.card_ID"))
+    session_ID: Mapped[int] = mapped_column(ForeignKey("session.session_ID"))
+    card_ID: Mapped[int] = mapped_column(ForeignKey("card.card_ID"))
     bid_amount: Mapped[Optional[int]]
     money_gain: Mapped[Optional[int]]
 
 class GameTypes(Base):
-    __tablename__ = "Game_types"
+    __tablename__ = "game_types"
 
     type_ID: Mapped[int] = mapped_column(primary_key=True)
     game_type: Mapped[str] = mapped_column(String(20))
     isSupervised: Mapped[int]
 
 class Tables(Base):
-    __tablename__ = "Tables"
+    __tablename__ = "tables"
 
     table_ID: Mapped[int] = mapped_column(primary_key=True)
-    type_ID: Mapped[int] = mapped_column(ForeignKey("Game_types.type_ID"))
+    type_ID: Mapped[int] = mapped_column(ForeignKey("game_types.type_ID"))
     balance: Mapped[Optional[int]]
     openning_time: Mapped[Optional[datetime.datetime]]
     closing_time: Mapped[Optional[datetime.datetime]]
@@ -134,6 +134,6 @@ class SessionTables(Base):
     __tablename__ = "session_tables"
 
     session_to_table_ID: Mapped[int] = mapped_column(primary_key=True)
-    session_ID: Mapped[int] = mapped_column(ForeignKey("Session.session_ID"))
-    table_ID: Mapped[int] = mapped_column(ForeignKey("Tables.table_ID"))
-    staff_ID: Mapped[Optional[int]] = mapped_column(ForeignKey("Dealers.staff_ID"))
+    session_ID: Mapped[int] = mapped_column(ForeignKey("session.session_ID"))
+    table_ID: Mapped[int] = mapped_column(ForeignKey("tables.table_ID"))
+    staff_ID: Mapped[Optional[int]] = mapped_column(ForeignKey("dealers.staff_ID"))
